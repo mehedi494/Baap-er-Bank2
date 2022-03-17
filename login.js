c3bcab909c872b26a8385e3994352b734574a471
MEHEDI MUNNA <ehasanmunna21@hotmail.com>
2022-03-17 15:29:43 +0600

    Log in page add

---
+++ login.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("btn")
+button.addEventListener("click", function () {
+    const emailText = document.getElementById('input-email')
+    const email = emailText.value
+    const passText = document.getElementById('input-pass')
+    const pass = passText.value
+    console.log(email, pass)
+    if (email == 'sontan@baap.com' && pass == 12345) {
+    
+        window.location.href = "./Banking.html";
+        console.log('ok')
+
+    }
+    else {
+        const errMsg = document.getElementById('err-msg').style.display = "block";
+        
+    }
+    // console.log(email,pass);
+})
